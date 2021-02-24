a9f3ffd08cbd2740ba4b767f2a40af81f080bbc4
Matthew Nitschke <matthewcnitschke@gmail.com>
2021-02-24 08:53:47 -0700

    implemented new delete icon and removing disabled query params

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,17 +1,25 @@
-function insertUrlParam(key, value) {
-	if (history.pushState) {
-		let searchParams = new URLSearchParams(window.location.search);
-		searchParams.set(key, value);
-		let newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${searchParams.toString()}`;
-		window.history.pushState({path: newurl}, '', newurl);
-	}
-}
-
 function injectParams(queryParams) {
+	if (!history.pushState) return;
+
 	console.log(`Injecting Parameters: ${JSON.stringify(queryParams)}`);
+
+	let searchParams = new URLSearchParams(window.location.search);
+
 	queryParams
 		.filter(param => param.enabled)
-		.forEach(({key, value}) => insertUrlParam(key, value))
+		.forEach(({key, value}) => searchParams.set(key, value))
+
+	queryParams
+		.filter(param => !param.enabled)
+		.forEach(({key, value}) => searchParams.delete(key, value))
+
+	let newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
+
+	if (searchParams.toString().trim().length > 0) {
+		newUrl += `?${searchParams.toString()}`
+	}
+
+	window.history.pushState({path: newUrl}, '', newUrl);
 }
 
 chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatches'], ({globalEnabled, queryParams, urlMatches}) => {
