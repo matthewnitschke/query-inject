fa7491b8f0593f7c26e268edf983a3bc44737239
Matthew Nitschke <matthewcnitschke@gmail.com>
2021-02-28 20:15:32 -0700

    added multiple matchers support

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,6 +1,4 @@
 function injectParams(queryParams) {
-	if (!history.pushState) return;
-
 	console.log(`Injecting Parameters: ${JSON.stringify(queryParams)}`);
 
 	let searchParams = new URLSearchParams(window.location.search);
@@ -8,11 +6,24 @@ function injectParams(queryParams) {
 	queryParams
 		.filter(param => param.enabled)
 		.forEach(({key, value}) => searchParams.set(key, value))
-
+	
 	queryParams
 		.filter(param => !param.enabled)
 		.forEach(({key, value}) => searchParams.delete(key, value))
 
+	setUrlSearchParams(searchParams);
+}
+
+function clearInjectedParams(queryParams) {
+	let searchParams = new URLSearchParams(window.location.search);
+	
+	queryParams
+		.forEach(({key, value}) => searchParams.delete(key, value))
+
+	setUrlSearchParams(searchParams);
+}
+
+function setUrlSearchParams(searchParams) {
 	let newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
 
 	if (searchParams.toString().trim().length > 0) {
@@ -22,15 +33,30 @@ function injectParams(queryParams) {
 	window.history.pushState({path: newUrl}, '', newUrl);
 }
 
-chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatches'], ({globalEnabled, queryParams, urlMatches}) => {
-    if (!globalEnabled || (urlMatches ?? '').length <= 0) {
+function doesUrlMatch(url, matchUrls) {
+	const enabledUrls = matchUrls
+		.filter(({enabled}) => enabled)
+		.map(({matchStr}) => matchStr);
+
+	const regexUrl = `(${enabledUrls.join(')|(')})`
+		.replace(/\//g, '\\/') // escape '/' charachters
+		.replace(/\*/g, '.*'); // convert '*' into '.*' for match all
+
+	return RegExp(`^${regexUrl}$`, 'i').test(url)
+}
+
+chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatchers'], ({globalEnabled, queryParams, urlMatchers}) => {
+	if (!history.pushState) return;
+
+	let matchers = (urlMatchers ?? []);
+
+    if (!globalEnabled || matchers.length <= 0) {
         return;
     }
 
-	let reg = new RegExp(urlMatches)
-	if (!window.location.href.match(reg)) {
-		return
+	if (doesUrlMatch(window.location.href, matchers)) {
+		injectParams(queryParams);
+	} else {
+		clearInjectedParams(queryParams);
 	}
-
-	injectParams(queryParams);
 });
--- src/options/script.js
+++ src/options/script.js
@@ -3,22 +3,37 @@ var app = new Vue({
     data: {
         globalEnabled: true,
         queryParams: [],
-        urlMatches: ''
+        urlMatchers: [
+            { enabled: true, matchStr: "http://www.google.com/*" }
+        ]
     },
     mounted: function() {
         this.load();
     },
     methods: {
-        add: function () {
+        addQueryParam: function () {
             this.queryParams.push({
                 enabled: true
             })
 
             this.save();
         },
-        remove: function(item) {
+        removeQueryParam: function(item) {
             let index = this.queryParams.indexOf(item)
             this.queryParams.splice(index, 1)
+
+            this.save();
+        },
+        addMatcher: function() {
+            this.urlMatchers.push({
+                enabled: true,
+            });
+            
+            this.save();
+        },
+        removeUrlMatcher: function(item) {
+            let index = this.urlMatchers.indexOf(item)
+            this.urlMatchers.splice(index, 1)
 
             this.save();
         },
@@ -26,14 +41,14 @@ var app = new Vue({
             chrome.storage.sync.set({
                 globalEnabled: this.globalEnabled,
                 queryParams: this.queryParams,
-                urlMatches: this.urlMatches
+                urlMatchers: this.urlMatchers
             });
         },
         load: function() {
-            chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatches'], ({globalEnabled, queryParams, urlMatches}) => {
+            chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatchers'], ({globalEnabled, queryParams, urlMatchers}) => {
                 this.globalEnabled = globalEnabled ?? this.globalEnabled;
                 this.queryParams = queryParams ?? this.queryParams;
-                this.urlMatches = urlMatches ?? this.urlMatches;
+                this.urlMatchers = urlMatchers ?? this.urlMatchers;
             });            
         } 
     }
