15fda2ed18a8c4cef0d5746aa08c632b273e2160
Matthew Nitschke <matthewcnitschke@gmail.com>
2022-03-04 18:58:29 -0700

    implemented better console logging and duplicate value resilience

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,18 +1,70 @@
 function injectParams(queryParams) {
-	console.log(`Injecting Parameters: ${JSON.stringify(queryParams)}`);
+	let searchParams = new URLSearchParams(window.location.search);
 
+	console.groupCollapsed('Injecting Query Parameters');
+
+	let enabledParameters = queryParams
+		.filter(({enabled}) => enabled)
+		.reduce((acc, {key, value}) => {
+			searchParams.set(key, value);
+			console.log(`${key}: '${value}'`);
+			return [...acc, key];
+		}, []);
+
+	console.group("Params Deleted from URL");
+	queryParams
+		.filter(({key, enabled}) => !enabled && !enabledParameters.hasOwnProperty(key))
+		.forEach(({key, value}) => {
+			searchParams.delete(key, value);
+			console.log(`${key}: '${value}'`)
+		});
+	
+	console.groupEnd();
+	console.groupEnd();
+
+	setUrlSearchParams(searchParams);
+}
+
+function clearInjectedParams(queryParams) {
 	let searchParams = new URLSearchParams(window.location.search);
 
 	queryParams
-		.filter(param => param.enabled)
-		.forEach(({key, value}) => searchParams.set(key, value))
-	
-	queryParams
-		.filter(param => !param.enabled)
 		.forEach(({key, value}) => searchParams.delete(key, value))
 
 	setUrlSearchParams(searchParams);
 }
+
+chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatchers'], ({globalEnabled, queryParams, urlMatchers}) => {
+	if (!history.pushState) return;
+
+	let matchers = (urlMatchers ?? []);
+
+    if (!globalEnabled || matchers.length <= 0) {
+        return;
+    }
+
+	const {
+		enabledUrls,
+		disabledUrls,
+	} = urlMatchers.reduce((acc, {enabled, matchStr}) => {
+		if (enabled) {
+			acc.enabledUrls.push(matchStr)
+		} else {
+			acc.disabledUrls.push(matchStr)
+		}
+		return acc
+	}, {enabledUrls: [], disabledUrls: []});
+
+	if (doesUrlMatch(window.location.href, enabledUrls)) {
+		// if the current url matches any of the enabledUrls, inject the params
+		injectParams(queryParams);
+	} else if (doesUrlMatch(window.location.href, disabledUrls)) {
+		// if the current url matches any of the disabledUrls (and non of the enabled urls), clear all params
+		clearInjectedParams(queryParams)
+	}
+});
+
+// ------------------------------------ Utils ------------------------------------
 
 function setUrlSearchParams(searchParams) {
 	let newUrl = `${window.location.protocol}//${window.location.host}${window.location.pathname}`;
@@ -34,39 +86,3 @@ function doesUrlMatch(url, matchUrls) {
 
 	return RegExp(`^${regexUrl}$`, 'i').test(url)
 }
-
-function clearInjectedParams(queryParams) {
-	let searchParams = new URLSearchParams(window.location.search);
-
-	queryParams
-		.forEach(({key, value}) => searchParams.delete(key, value))
-
-	setUrlSearchParams(searchParams);
-}
-
-chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatchers'], ({globalEnabled, queryParams, urlMatchers}) => {
-	if (!history.pushState) return;
-
-	let matchers = (urlMatchers ?? []);
-
-    if (!globalEnabled || matchers.length <= 0) {
-        return;
-    }
-
-	const {enabledUrls, disabledUrls} = urlMatchers.reduce((acc, {enabled, matchStr}) => {
-		if (enabled) {
-			acc.enabledUrls.push(matchStr)
-		} else {
-			acc.disabledUrls.push(matchStr)
-		}
-		return acc
-	}, {enabledUrls: [], disabledUrls: []});
-
-	if (doesUrlMatch(window.location.href, enabledUrls)) {
-		// if the current url matches any of the enabledUrls, inject the params
-		injectParams(queryParams);
-	} else if (doesUrlMatch(window.location.href, disabledUrls)) {
-		// if the current url matches any of the disabledUrls (and non of the enabled urls), clear all params
-		clearInjectedParams(queryParams)
-	}
-});
