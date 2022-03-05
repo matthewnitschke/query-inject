deedbc4567f5268f6a34d9a66db4f562b01853f8
Matthew Nitschke <matthewcnitschke@gmail.com>
2022-03-04 19:28:33 -0700

    piviot on query inject design

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,86 @@
+chrome.storage.sync.get([
+	'globalEnabled',
+	'queryParams',
+	'urlMatchers',
+], ({
+	globalEnabled,
+	queryParams,
+	urlMatchers,
+}) => {
+	if (!history.pushState) return;
+
+	let matchers = (urlMatchers ?? []);
+
+    if (!globalEnabled || matchers.length <= 0) {
+        return;
+    }
+
+	// Parse the params
+	const {
+		paramsToAdd,
+		paramsToDelete
+	} = parseParameters(window.location.href, matchers, queryParams);
+	
+	// Log what will happen
+	console.groupCollapsed('Query Inject');
+	console.log(JSON.stringify({paramsToAdd, paramsToDelete}, null, 2));
+	console.groupEnd();
+
+	// Preform the injection/exclusion
+	let searchParams = new URLSearchParams(window.location.search);
+	paramsToAdd.forEach(({key, value}) => searchParams.set(key, value));
+	paramsToDelete.forEach(({key}) => searchParams.delete(key));
+	setUrlSearchParams(searchParams);
+});
+
+function parseParameters(
+	currentUrl,
+	urlMatchers,
+	queryParams,
+) {
+	const {
+		enabledUrls,
+		disabledUrls,
+	} = urlMatchers.reduce((acc, {enabled, matchStr}) => {
+		if (enabled) {
+			acc.enabledUrls.push(matchStr)
+		} else {
+			acc.disabledUrls.push(matchStr)
+		}
+		return acc
+	}, {enabledUrls: [], disabledUrls: []});
+
+
+	if (doesUrlMatch(currentUrl, enabledUrls)) {		
+		const {
+			enabledParams,
+			disabledParams
+		} = queryParams.reduce((acc, {enabled, key, value}) => {
+			if (enabled) {
+				acc.enabledParams[key] = value
+			} else {
+				acc.disabledParams[key] = value
+			}
+		}, {enabledParams: {}, disabledParams: {}});
+
+		let paramsToDelete = Object.keys(disabledParams)
+			.filter(key => !enabledParams.hasOwnProperty(key))
+			.reduce((acc, key) => (res[key] = disabledParams[key]), {})
+
+		return {
+			paramsToAdd: enabledParams,
+			paramsToDelete
+		}
+
+	} else if (doesUrlMatch(currentUrl, disabledUrls)) {
+		// if the current url matches any of the disabledUrls (and non of the enabled urls), clear all params
+		return {
+			paramsToAdd: {},
+			paramsToDelete: queryParams.reduce((acc, {key, value}) => ({...acc, [key]: value}), {})
+		}
+	}
+}
+
 function injectParams(queryParams) {
 	let searchParams = new URLSearchParams(window.location.search);
 
@@ -11,7 +94,7 @@ function injectParams(queryParams) {
 			return [...acc, key];
 		}, []);
 
-	console.group("Params Deleted from URL");
+	console.groupCollapsed("Params Deleted from URL");
 	queryParams
 		.filter(({key, enabled}) => !enabled && !enabledParameters.hasOwnProperty(key))
 		.forEach(({key, value}) => {
@@ -24,45 +107,6 @@ function injectParams(queryParams) {
 
 	setUrlSearchParams(searchParams);
 }
-
-function clearInjectedParams(queryParams) {
-	let searchParams = new URLSearchParams(window.location.search);
-
-	queryParams
-		.forEach(({key, value}) => searchParams.delete(key, value))
-
-	setUrlSearchParams(searchParams);
-}
-
-chrome.storage.sync.get(['globalEnabled', 'queryParams', 'urlMatchers'], ({globalEnabled, queryParams, urlMatchers}) => {
-	if (!history.pushState) return;
-
-	let matchers = (urlMatchers ?? []);
-
-    if (!globalEnabled || matchers.length <= 0) {
-        return;
-    }
-
-	const {
-		enabledUrls,
-		disabledUrls,
-	} = urlMatchers.reduce((acc, {enabled, matchStr}) => {
-		if (enabled) {
-			acc.enabledUrls.push(matchStr)
-		} else {
-			acc.disabledUrls.push(matchStr)
-		}
-		return acc
-	}, {enabledUrls: [], disabledUrls: []});
-
-	if (doesUrlMatch(window.location.href, enabledUrls)) {
-		// if the current url matches any of the enabledUrls, inject the params
-		injectParams(queryParams);
-	} else if (doesUrlMatch(window.location.href, disabledUrls)) {
-		// if the current url matches any of the disabledUrls (and non of the enabled urls), clear all params
-		clearInjectedParams(queryParams)
-	}
-});
 
 // ------------------------------------ Utils ------------------------------------
 
